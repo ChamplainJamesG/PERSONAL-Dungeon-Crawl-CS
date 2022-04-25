08b1ca09804bb7789ff3c86a0f5d6872d1196688
ChamplainJamesG <35664326+ChamplainJamesG@users.noreply.github.com>
2022-04-25 17:48:55 -0400

    Adding tiles

---
+++ Loop/Assets/Scripts/Tile/TileComp.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileComp : MonoBehaviour
+{
+    // Determine Where I connect to
+    // Remove doors.
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
